3c8561e3c8abea0f1f73412b1bfc0e5ad9c591f3
RQuarx <RQuarx@protonmail.com>
2025-04-20 12:56:01 +0700

    Added license notice

--- src/tabs/packages.cc
+++ src/tabs/packages.cc
@@ -1,3 +1,22 @@
+/**
+ * @file tabs/packages.cc
+ *
+ * This file is part of AURGH
+ *
+ * AURGH is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * AURGH is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <gtkmm-3.0/gtkmm/scrolledwindow.h>
 #include <gtkmm-3.0/gtkmm/comboboxtext.h>
 #include <gtkmm-3.0/gtkmm/searchentry.h>
