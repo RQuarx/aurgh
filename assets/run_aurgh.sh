75dae645d203d172a020d25099acd3dcabeccaaf
RQuarx <RQuarx@protonmail.com>
2025-05-28 17:52:17 +0700

    fix: Tried to fix the GTK3 action.

--- assets/run_aurgh.sh
+++ assets/run_aurgh.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-xdg_cache_home=$XDG_CACHE_HOME
-
-rm $XDG_CACHE_HOME/aurgh/log.txt
-aurgh -l $XDG_CACHE_HOME/aurgh/log.txt
+rm "$XDG_CACHE_HOME"/aurgh/log.txt
+aurgh -l "$XDG_CACHE_HOME"/aurgh/log.txt
